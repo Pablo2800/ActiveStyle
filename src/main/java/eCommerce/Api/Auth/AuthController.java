3d50fd5004f8089a26adef3d5f919e3666f16091
unknown <sebapine3004@gmail.com>
2024-04-05 17:38:53 -0300

    Cross

--- src/main/java/eCommerce/Api/Auth/AuthController.java
+++ src/main/java/eCommerce/Api/Auth/AuthController.java
@@ -6,13 +6,11 @@ import eCommerce.Api.RegisterRequest.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin("*")
 @RequestMapping("/auth")
 public class AuthController {
 
--- src/main/java/eCommerce/Api/Controllers/UsuarioController.java
+++ src/main/java/eCommerce/Api/Controllers/UsuarioController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value= "/api/v1/usuario")
+@CrossOrigin("*")
 @RequiredArgsConstructor
 public class UsuarioController{
     private final UsuarioService usuarioService;
