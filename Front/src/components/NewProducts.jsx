d1c4ede30c035c0bc008f82de715f8ca3d93a8ea
Pablo2800 <pablopalazzetti@gmail.com>
2024-04-29 18:38:54 -0300

    arreglos

--- Front/src/components/NewProducts.jsx
+++ Front/src/components/NewProducts.jsx
@@ -5,26 +5,44 @@ export default function NewProducts() {
     {
       id: 1,
       url: "https://nikearprod.vtexassets.com/arquivos/ids/793832-1000-1000?v=638379223305770000&width=1000&height=1000&aspect=true",
+      description: "Nike Air Force 1 07 LV8",
+      category: "Zapatillas para Hombre",
+      price: "$239.999",
     },
     {
       id: 2,
       url: "https://nikearprod.vtexassets.com/arquivos/ids/880628-1000-1000?v=638467320930270000&width=1000&height=1000&aspect=true",
+      description: "Nike Air Force 1 07 LV8",
+      category: "Zapatillas para Hombre",
+      price: "$239.999",
     },
     {
       id: 3,
       url: "https://nikearprod.vtexassets.com/arquivos/ids/794168-1000-1000?v=638379227989030000&width=1000&height=1000&aspect=true",
+      description: "Nike Air Force 1 07 LV8",
+      category: "Zapatillas para Hombre",
+      price: "$239.999",
     },
     {
       id: 4,
       url: "https://nikearprod.vtexassets.com/arquivos/ids/762481-1000-1000?v=638316091353670000&width=1000&height=1000&aspect=true",
+      description: "Nike Air Force 1 07 LV8",
+      category: "Zapatillas para Hombre",
+      price: "$239.999",
     },
     {
       id: 5,
       url: "https://nikearprod.vtexassets.com/arquivos/ids/762481-1000-1000?v=638316091353670000&width=1000&height=1000&aspect=true",
+      description: "Nike Air Force 1 07 LV8",
+      category: "Zapatillas para Hombre",
+      price: "$239.999",
     },
     {
       id: 6,
       url: "https://nikearprod.vtexassets.com/arquivos/ids/762481-1000-1000?v=638316091353670000&width=1000&height=1000&aspect=true",
+      description: "Nike Air Force 1 07 LV8",
+      category: "Zapatillas para Hombre",
+      price: "$239.999",
     },
   ];
   const scrollContainer = useRef(null);
@@ -63,12 +81,15 @@ export default function NewProducts() {
         <div className="flex">
           {products.map((product) => {
             return (
-              <div key={product.id} className="p-2 w-72">
+              <div key={product.id} className="p-2 w-72 h-96 flex flex-col">
                 <img
                   src={product.url}
                   alt=""
                   className="w-full h-full object-cover"
                 />
+                <p className="text-lg font-bold">{product.description}</p>
+                <p className="text-gray-600">{product.category}</p>
+                <p className="font-bold">{product.price}</p>
               </div>
             );
           })}
