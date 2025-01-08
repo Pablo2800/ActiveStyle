b3b42fb8b4c2d2c5b80eb34b812a8ac2f08d0de3
unknown <sebapine3004@gmail.com>
2025-01-08 17:26:01 -0300

    Actualizacion Response Login

--- src/main/java/eCommerce/Api/Auth/AuthResponseLogin.java
+++ src/main/java/eCommerce/Api/Auth/AuthResponseLogin.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthResponseLogin {
+    Long id;
     String username;
     String firstname;
     String lastname;
